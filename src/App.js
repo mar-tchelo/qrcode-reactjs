e0264ee80f8c1c50e7ff1b6a253a15afcdec84a7
mar-tchelo <marcelo.js10@gmail.com>
2022-09-21 18:35:57 -0300

    Download qr Code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import { useState } from 'react';
 import QRCode from 'react-qr-code';
+import QRCodeLink from 'qrcode';
 import './App.css';
 
 function App() {
 
   const [link, setLink] = useState('')
+  const [qrcodeLink, setqrcodeLink] = useState('')
+
+  function handleGenereter(link_url){
+    QRCodeLink.toDataURL(link_url, {
+      width: 600,
+      margin:3, 
+     }, function(err, url){
+      setqrcodeLink(url)
+    })
+  }
 
   function handleQrcode(e) {
     setLink(e.target.value)
+    handleGenereter(e.target.value)
   }
 
   return (
@@ -22,7 +34,10 @@ function App() {
         placeholder='Digite seu link'
         value={link}
         onChange={(e) => handleQrcode(e)}
+        required={true}
       />
+
+      <a href={qrcodeLink} download={`qrcode.jpg`}>Baixar QRCode!</a>
     </div>
 
     
