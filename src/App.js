a071c7750e10a35f9cc2995cc355ec26539938b2
mar-tchelo <marcelo.js10@gmail.com>
2022-09-21 18:10:22 -0300

    qrCode generator dynamic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-
+import { useState } from 'react';
+import QRCode from 'react-qr-code';
 import './App.css';
 
 function App() {
+
+  const [link, setLink] = useState('')
+
+  function handleQrcode(e) {
+    setLink(e.target.value)
+  }
+
   return (
     <div className="container">
+
+      <QRCode
+        value={link}
+      />
+
       <input
         className='input'
-        placeholder='Gigite seu link'
+        placeholder='Digite seu link'
+        value={link}
+        onChange={(e) => handleQrcode(e)}
       />
     </div>
+
+    
   );
 }
 
